074f09b986f6bfafdf5018be962e1dd7e5c4592a
User -2023WXXOU\Administrator <1000tmddms@gmail.com>
2023-12-20 11:38:34 +0900

    useEffect!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
@@ -19,13 +19,20 @@ function App() {
     //버튼을 클릭할때마다 상위 콜백함수가 실행되는데 내부에서 태어나는 count는 이전의 week이 가진 과거연산합계에 + 1 한 값으로 태어나므로 밑에 weeklist[week]의 week 값이 절대 이전과 같을수 없다.
   }
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      weekUpdate((week + 1) & weeklist.length);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, [week]);
+
   //const weekUpdate = () =>{weekchange((week+1)%weeklist.length)}
 
 
   return (
     <div className="App">
 
-      <button onClick={weekUpdate}>클릭</button>
+      <button onClick={weekUpdate}>클릭하면 요일이 넘어가거나 가만히 둬도 넘어감</button>
       <p>{weeklist[week]}요일</p>
     </div>
   );
@@ -40,5 +47,5 @@ export default App;
 //별코딩방법
 //const weekUpdate = () =>{
 // let count;
-// if ( count = 7 ){ count = 0 ; } else { count + 1 ; }
+// if ( count >= 7 ){ count = 0 ; } else { count + 1 ; }
 // }
