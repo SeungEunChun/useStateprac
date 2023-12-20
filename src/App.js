43ea4cf3a0daa0bb724eba954fc5750387bcd2b4
User -2023WXXOU\Administrator <1000tmddms@gmail.com>
2023-12-20 10:28:51 +0900

    useState로 값이 증가하고 순환하는 함수 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [week, weekchange] = useState(0);
+  const weeklist = ["월", "화", "수", "목", "금", "토", "일"]
+  const weekUpdate = (num) => {
+
+
+    weekchange(num % 7); //week의 변수가 변경되고 랜더링됨
+    //useState 변경함수만 연산식이 가능하다.
+
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={() => { weekUpdate(week + 1) }}>클릭</button>
+      <p>{weeklist[week]}요일</p>
     </div>
   );
 }
