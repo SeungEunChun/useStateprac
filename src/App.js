3bf0b4e4c451d109554c9d4e70913d4b2fe65ad2
User -2023WXXOU\Administrator <1000tmddms@gmail.com>
2023-12-20 10:56:01 +0900

    새로 선언된 변수가 weeklist 안의 갯수와 같으면 0으로 돌아가야하므로 나누기 들어감

--- src/App.js
+++ src/App.js
@@ -5,23 +5,40 @@ import './App.css';
 function App() {
   const [week, weekchange] = useState(0);
   const weeklist = ["월", "화", "수", "목", "금", "토", "일"]
-  const weekUpdate = (num) => {
+  // const weekUpdate = (num) => {
 
 
-    weekchange(num % 7); //week의 변수가 변경되고 랜더링됨
-    //useState 변경함수만 연산식이 가능하다.
+  //   weekchange(num % 7); //week의 변수가 변경되고 랜더링됨
+  //   //useState 변경함수만 연산식이 가능하다.
 
 
+  // }
+  const weekUpdate = () => {
+    let count = (week + 1) % weeklist.length;
+    weekchange(count);
+    //버튼을 클릭할때마다 상위 콜백함수가 실행되는데 내부에서 태어나는 count는 이전의 week이 가진 과거연산합계에 + 1 한 값으로 태어나므로 밑에 weeklist[week]의 week 값이 절대 이전과 같을수 없다.
   }
+
+  //const weekUpdate = () =>{weekchange((week+1)%weeklist.length)}
 
 
   return (
     <div className="App">
 
-      <button onClick={() => { weekUpdate(week + 1) }}>클릭</button>
+      <button onClick={weekUpdate}>클릭</button>
       <p>{weeklist[week]}요일</p>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+//별코딩방법
+//const weekUpdate = () =>{
+// let count;
+// if ( count = 7 ){ count = 0 ; } else { count + 1 ; }
+// }
